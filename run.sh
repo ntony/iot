42cd92c78a804712c6621291fb64cc0118f824af
nidhintony <nidhintony@gmail.com>
2018-03-25 22:19:46 +0530

    script bug fix.

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ git pull
 echo "Building Jar"
 mvn clean install -DskipTests=true
 echo "Running IOT"
+killall java
 java -Xmx700m -Xms256m -jar -Djava.security.egd=file:/dev/./urandom  target/iot-0.0.1-SNAPSHOT.jar &
 
 
